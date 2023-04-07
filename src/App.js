3f1330dca4216760398c69076eb341ed2747aab8
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-04-07 14:09:23 +0530

    Home page setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
-
+import Home from "./pages/Home";
+import {Routes, Route} from "react-router-dom";
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 

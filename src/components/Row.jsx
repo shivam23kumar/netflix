aa080239588f3cd4b297eefb80b29e6c46bf566e
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-04-09 12:33:51 +0530

    Movie part done

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,25 +1,31 @@
 import React, {useState,useEffect} from 'react';
 import axios from "axios";
+import Movie from "./Movie";
+
 
 const Row = ({title,fetchURL}) => {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
   useEffect(() => {
-    axios.get(fetchURL).then((response) =>{
+    const fetchMovie = async ()=>{
+    try{
+      const response = await axios.get(fetchURL)
       setMovies(response.data.results)
-    })
-  }, [fetchURL]);
+    }catch(error){
+      console.error("Error fetching movies data", error);
+    }
+    
+  };
+  fetchMovie();
+  },[fetchURL]);
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
         <div className="relative flex items-center">
-          <div id={"slider"}>
-            {movies.map((item)=>{
-              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-              </div>
-              
-              
-            })}
+          <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+            {movies.map((item, id)=>(
+              <Movie key={id} item={item}/>
+            ))}
 
           </div>
         </div>
